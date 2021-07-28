7c029b2823f347d4bbdc345379b6ea8e533e8f09
nojuskybartas <nojus.kybartas@gmail.com>
2021-07-28 10:25:08 +0200

    Setup a saving function
    Saving the badges into memory to later use as training data

--- app/badge-detector/main.py
+++ app/badge-detector/main.py
@@ -7,7 +7,7 @@ PATH_TO_RTSP_HIKVSION_DOME_CAMERA = 'rtsp://kplr:5hVUlm3S7o92@10.10.12.241/Strea
 # Increasing any of these values results in a better accuracy, however slower speeds
 
 BUFFER = 3  # max image buffer capacity
-OBJECT_LIFETIME = 10  # How long should the tracker still try to find a lost tracked person (measured in frames)
+OBJECT_LIFETIME = 2  # How long should the tracker still try to find a lost tracked person (measured in frames)
 MAX_BADGE_CHECK_COUNT = 3  # How many times a full BUFFER should be checked before a person is declared to be an imposter
 
 person_detection_model = PersonDetector().model
@@ -18,7 +18,7 @@ badge_detection_model = BadgeDetector().model
 # camera3 = SurveillanceCamera(3, person_detection_model, badge_detection_model, PATH_TO_MULTI_PERSON_TEST_VIDEO, 25, 3, BUFFER, OBJECT_LIFETIME, MAX_BADGE_CHECK_COUNT, record='output')
 # goproCam = SurveillanceCamera('GoPro', person_detection_model, badge_detection_model, 'udp://127.0.0.1:10000', 30, 10, BUFFER, OBJECT_LIFETIME, MAX_BADGE_CHECK_COUNT, interface = True, record='output')
 hikvision = SurveillanceCamera('labs-hikvision', person_detection_model, badge_detection_model, PATH_TO_RTSP_HIKVSION_DOME_CAMERA, 10,
-                               5, BUFFER, OBJECT_LIFETIME, MAX_BADGE_CHECK_COUNT, record=os.path.join('output', 'recordings'))
+                               2, BUFFER, OBJECT_LIFETIME, MAX_BADGE_CHECK_COUNT, record=os.path.join('output', 'recordings'))
 
 camera_list = [hikvision]
 
